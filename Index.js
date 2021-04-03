f8ecd5f6734604eb97ad5f2eee73122d8ffccec4
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-04-02 22:51:26 -0400

    finished all the prompts for the manager, intern, engineer and returned out of function when the option is selected that states all employees have been added.

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ const inquirer = require("inquirer");
 const Manager = require("./src/Manager");
 const Engineer = require("./src/Engineer");
 const Intern = require("./src/Intern");
-const generate = require("./src/generatedHtml");
+const generate = require("./src/generateHtml");
 const fs = require('fs');
 
 const employees = [];
@@ -10,7 +10,7 @@ const employeeType = [
     {
         type: "list",
         Message: "Please select the type of employee that you are adding to your team",
-        choices: ["Manager", "Engineer", "Intern"],
+        choices: ["Manager", "Engineer", "Intern", "I have added all of the employees"],
         name: "position",
     },
     
@@ -102,13 +102,31 @@ inquirer
         inquirer.prompt(managerQuestions)
         .then((managerResponses) => {
             const manager = new Manager(managerResponses.name, managerResponses.ID, managerResponses.email, managerResponses.officeNumber)
-            employees.push(manager)
+            employees.push(manager);
             console.log(manager);
-            
+            employeeInfo ();
         }
+    )
+} else if (answers.position === "Engineer") {
+        inquirer.prompt(engineerQuestions)
+        .then((engineerResponses) => {
+            const engineer = new Engineer(engineerResponses.name, engineerResponses.ID, engineerResponses.email, engineerResponses.gitHub)
+            employees.push(engineer);
+            console.log(engineer);
+            employeeInfo ();
+    })
+} else if (answers.position === "Intern") {
+    inquirer.prompt(internQuestions)
+    .then((internResponses) => {
+        const intern = new Intern(internResponses.name, internResponses.ID, internResponses.email, internResponses.school)
+        employees.push(intern);
+        console.log(intern);
+        employeeInfo ();
+})
+} else if (answers.position === "I have added all of the employees") {
+    return;    
+}
+  }
     )}
-    }
-)};
-
 
 employeeInfo ();
