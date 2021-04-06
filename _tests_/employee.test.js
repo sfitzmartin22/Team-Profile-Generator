c8330c92e0c67a725351aeb088e7a89e463e36bf
Sean Fitzmartin <sfitzmartin22@gmail.com>
2021-04-05 23:07:55 -0400

    setup 2 of the employee tests that are succesfully passing

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -1,1 +1,15 @@
-const Employee = require("./lib/Employee");
+const { test } = require("@jest/globals");
+const Employee = require("../lib/Employee");
+
+test("Checking that an object is created", () => {
+    const employee = new Employee();
+    expect(typeof(employee)).toBe("object");
+});
+
+test("checking that the values in the object are pulling correctly", () => {
+    const name = "Sean Fitzmartin";
+    const ID = "22";
+    const email = "sfitzmartin22@gmail.com";
+
+    const objectValues = new Employee (name, ID, email);
+});
